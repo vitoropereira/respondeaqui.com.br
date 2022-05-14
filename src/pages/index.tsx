86b461f5132a66bc1fe3bb7330d6f944d8a7882d
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2022-05-14 12:15:28 -0300

    Putting the data search via server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,23 @@ import { useState, useEffect } from "react";
 
 import collaborators from "../../public/init/collaborators.json";
 import CollaboratorsCard from "../components/CollaboratorsCard";
+import { GetStaticProps } from "next";
 
-export default function Page() {
+export interface OwnerProps {
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  url: string;
+  html_url: string;
+  repos_url: string;
+}
+
+interface RepositoryProps {
+  owner: OwnerProps;
+}
+
+export default function Page({ imagesUrls }) {
+  // console.log(imagesUrls);
   const [shuffledCollaborators, setShuffledCollaborators] = useState([]);
   const [confettiWidth, setConfettiWidth] = useState(0);
   const [confettiHeight, setConfettiHeight] = useState(0);
@@ -50,6 +65,7 @@ export default function Page() {
               key={username}
               username={username}
               layoutId={username}
+              imagesUrls={imagesUrls}
             />
           );
         })}
@@ -66,3 +82,22 @@ function shuffle(arr) {
   }
   return newArr;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const shuffleCollaborators = shuffle(collaborators);
+  const imagesUrls = shuffleCollaborators.forEach(async (username: string) => {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/repos`
+    );
+
+    const data = await response.json();
+    console.log(data);
+  });
+
+  console.log("imagesUrls");
+  console.log(imagesUrls);
+  return {
+    props: {},
+    revalidate: 1,
+  };
+};
