367bf318cdaffe76eee581ce3b9d6aaa7a6225d1
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2022-12-30 14:53:37 -0300

    :rocket: To Deploy

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -8,16 +8,16 @@ export function Marquee({ text }: Marquee) {
   const [currentText, setCurrentText] = useState(text);
   const [intervalId, setIntervalId] = useState<number>(undefined);
 
-  useEffect(() => {
-    const id = setInterval(() => {
-      setCurrentText((current) => {
-        return current.substring(1) + current[0];
-      });
-    }, 3000);
-    setIntervalId(id);
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     setCurrentText((current) => {
+  //       return current.substring(1) + current[0];
+  //     });
+  //   }, 3000);
+  //   setIntervalId(id);
 
-    return () => clearInterval(intervalId);
-  }, [intervalId]);
+  //   return () => clearInterval(intervalId);
+  // }, [intervalId]);
 
   return <div className="text-sm font-bold text-left">{currentText}</div>;
 }
