fe6e67014e4037daab4363638e57bc5f67cb1895
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2023-10-01 00:21:51 -0300

    :heavy_plus_sign: Added `Clarity` and `Google Tag Manager`

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Head,  Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,7 +10,29 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-br">
-        <Head />
+        <Head>
+        <Script id="clarity-analytics">
+          {` 
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "j3ke3jcpnr");
+          `}
+        </Script>
+
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-1H84XKW7NW" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-1H84XKW7NW');
+        `}
+        </Script>
+        
+        </Head>
         <body>
           <Main />
           <NextScript />
